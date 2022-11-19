454829a881f3d6f5f5d67b48b9527268cb399d8d
Aung Thu Hein <ahein8604@gmail.com>
2022-11-19 18:25:24 +0630

    emailvirify_&_createTable

---
+++ database/migrations/2022_11_19_111159_create_posts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId("community_id")->constrained();//(foreignId,constrained) က dataBase relasitionship လုပ်တဲ့ နေရာမှာ သုံးပါတယ်။
+            $table->foreignId("user_id")->constrained();
+            $table->string("title")->unique();
+            $table->text("url");
+            $table->string("slug");
+            $table->longText("description");
+            $table->softDeletes();//delete လုပ် တဲ့ အခါ မှာ time ကို ပြပေးပါတယ်။
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+};
