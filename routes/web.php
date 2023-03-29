a727214692290403031e19b3b0d3fa4975516de0
Aung Thu Hein <100287026+aHein007@users.noreply.github.com>
2023-03-29 17:37:05 +0630

    prepare for project

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
-use Illuminate\Foundation\Application;
-use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Foundation\Application;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Backend\CommunityController;
 
 /*
 |--------------------------------------------------------------------------
@@ -25,14 +26,23 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::group(['middleware' => ['auth','verified']],function(){
+    Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+    })->name('dashboard');
+
+    Route::resource('communities',[CommunityController::class]);
+
+});
+
+
 
 require __DIR__.'/auth.php';
